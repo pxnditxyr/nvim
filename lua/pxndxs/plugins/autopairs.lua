eb7b8ef8dba1867bacf1ac9e9c089284ad272a65
Yuki xD <pxnditxyr@gmail.com>
2023-08-07 23:40:17 -0400

    feat: added autopairs plugin for complete (, {, <, etc.

---
+++ lua/pxndxs/plugins/autopairs.lua
@@ -0,0 +1,14 @@
+return {
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  config = function ()
+    require( 'nvim-autopairs' ).setup({
+      check_ts = true,
+      ts_config = {
+        lua = { 'string' },
+        javascript = { 'template_string' },
+        java = false,
+      }
+    })
+  end
+}
