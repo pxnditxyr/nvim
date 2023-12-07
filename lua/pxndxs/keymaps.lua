2485dae64ba67ac83e592d58bc03b1f7b193d42e
Yuki xD <pxnditxyr@gmail.com>
2023-12-07 10:21:58 -0400

    feat(keymaps): 🚀✨ Added new keymaps for handling tabs and splits 🌐🔗

--- lua/pxndxs/keymaps.lua
+++ lua/pxndxs/keymaps.lua
@@ -1,54 +1,58 @@
 
-local opts = {
-  noremap = true,
-  silent = true
-}
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap
 
-vim.keymap.set( 'n', '<A-j>', ':m .+1<CR>==', opts )
-vim.keymap.set( 'n', '<A-k>', ':m .-2<CR>==', opts )
-vim.keymap.set( 'v', '<A-j>', ':m \'>+1<CR>gv=gv', opts )
-vim.keymap.set( 'v', '<A-k>', ':m \'<-2<CR>gv=gv', opts )
+keymap.set( 'n', '<A-j>', ':m .+1<Return>==', opts )
+keymap.set( 'n', '<A-k>', ':m .-2<Return>==', opts )
+keymap.set( 'v', '<A-j>', ':m \'>+1<Return>gv=gv', opts )
+keymap.set( 'v', '<A-k>', ':m \'<-2<Return>gv=gv', opts )
 
 -- join lines
-vim.keymap.set( 'n', 'J', 'mzJ`z', opts )
+keymap.set( 'n', 'J', 'mzJ`z', opts )
 
 -- move in file
-vim.keymap.set( 'n', '<C-d>', '<C-d>zz', opts )
-vim.keymap.set( 'n', '<C-u>', '<C-u>zz', opts )
+keymap.set( 'n', '<C-d>', '<C-d>zz', opts )
+keymap.set( 'n', '<C-u>', '<C-u>zz', opts )
 
 -- in search after press n cursor will be in the middle of the screen
-vim.keymap.set( 'n', 'n', 'nzzzv', opts )
-vim.keymap.set( 'n', 'N', 'Nzzzv', opts )
+keymap.set( 'n', 'n', 'nzzzv', opts )
+keymap.set( 'n', 'N', 'Nzzzv', opts )
 
 -- search and replace
-vim.keymap.set( 'n', '<leader>fw', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', opts )
-vim.keymap.set( 'n', '<leader>fr', ':%s///gc<Left><Left><Left><Left>', opts )
+keymap.set( 'n', '<leader>fw', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', opts )
+keymap.set( 'n', '<leader>fr', ':%s///gc<Left><Left><Left><Left>', opts )
 
 -- best paste
-vim.keymap.set( 'x', '<leader>p', '"_dP', opts )
+keymap.set( 'x', '<leader>p', '"_dP', opts )
 
 -- copy and cut
-vim.keymap.set( 'n', '<leader>aa', ':%y<CR>',  opts )
-vim.keymap.set( 'n', '<leader>a',  ':%d<CR>',  opts )
-vim.keymap.set( 'n', '<leader>ch', ':%d<CR>i', opts )
+keymap.set( 'n', '<leader>aa', ':%y<Return>',  opts )
+keymap.set( 'n', '<leader>a',  ':%d<Return>',  opts )
+keymap.set( 'n', '<leader>ch', ':%d<Return>i', opts )
 
 -- put  in the end of line
-vim.keymap.set( 'n', '<leader>', 'A<ESC>j', opts )
+keymap.set( 'n', '<leader>', 'A<ESC>j', opts )
 
 -- separate parenthesis
-vim.keymap.set( 'n', '<leader>par', 'a<Space><Esc>h%i<Space><Esc>l%', opts )
+keymap.set( 'n', '<leader>par', 'a<Space><Esc>h%i<Space><Esc>l%', opts )
 
 -- save and quit
-vim.keymap.set( 'n', '<leader>w', ':w<CR>', opts )
-vim.keymap.set( 'n', '<leader>v', ':q<CR>', opts )
+keymap.set( 'n', '<leader>w', ':w<Return>', opts )
+keymap.set( 'n', '<leader>v', ':q<Return>', opts )
 
 -- execute node file
-vim.keymap.set( 'n', '<leader>bun', ':!bun %<CR>',    opts )
-vim.keymap.set( 'n', '<leader>py',   ':!python3 %<CR>', opts )
+keymap.set( 'n', '<leader>bun', ':!bun %<Return>',    opts )
+keymap.set( 'n', '<leader>py',   ':!python3 %<Return>', opts )
 
--- split window change
--- left
-vim.keymap.set( 'n', '<C-h>', '<C-w>h', opts )
-vim.keymap.set( 'n', '<C-l>', '<C-w>l', opts )
-vim.keymap.set( 'n', '<C-k>', '<C-w>k', opts )
-vim.keymap.set( 'n', '<C-j>', '<C-w>j', opts )
+-- Splits
+keymap.set( 'n', '<leader>vs', ':vsplit<Return>', opts )
+keymap.set( 'n', '<C-h>', '<C-w>h', opts )
+keymap.set( 'n', '<C-l>', '<C-w>l', opts )
+keymap.set( 'n', '<C-k>', '<C-w>k', opts )
+keymap.set( 'n', '<C-j>', '<C-w>j', opts )
+
+
+-- Tabs
+keymap.set( 'n', '<leader>tn', ':tabnew<Return>', opts )
+keymap.set( 'n', '<tab>',      ':tabnext<Return>', opts )
+keymap.set( 'n', '<S-tab>',    ':tabprevious<Return>', opts )
