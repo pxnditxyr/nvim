be3b17d230184919a72c3d1109d46223e86417f2
Pxndxs xD <pxnditxyr@gmail.com>
2024-04-03 09:27:30 -0400

    chore: disable additional Vim regex highlighting 🚫🔍✨

--- lua/pxndxs/plugins/treesitter.lua
+++ lua/pxndxs/plugins/treesitter.lua
@@ -19,13 +19,13 @@ return {
     })
     vim.g.skip_ts_commentstring_module = true
     configs.setup({
-      ensure_installed = { 'lua', 'vimdoc', 'vim', 'typescript', 'javascript', 'html', 'http', 'json' },
+      ensure_installed = { 'lua', 'vimdoc', 'vim', 'typescript', 'javascript', 'html', 'http', 'json', 'php' },
       ignore_install = {},
       sync_install = true,
       auto_install = true,
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = true,
+        additional_vim_regex_highlighting = false,
       },
       indent  = { enable = true },
       autotag = { enable = true },
